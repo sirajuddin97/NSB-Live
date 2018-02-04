2c94f6ed94f9f82f525353553fac2669317ce2bf
Sirajuddin Asjad <sirasjad@gmail.com>
2018-02-04 18:10:28 +0100

    Update

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ gi.require_version('Gtk', '3.0');
 gi.require_version('AppIndicator3', '0.1');
 from gi.repository import Gtk, AppIndicator3, GObject
 from threading import Thread
+from configparser import ConfigParser
 
 class Indicator():
     def __init__(self):
         self.app = 'show_proc';
-        self.currpath = os.path.dirname(os.path.realpath(__file__));
-        iconpath = self.currpath + '/img/nsb_small.png';
+        config = ConfigParser();
+        config.read('config.ini');
+        self.refresh_rate = config.getint('settings', 'refresh_rate');
+        self.station = config.getint('settings', 'station');
 
-        self.ind = AppIndicator3.Indicator.new(self.app, iconpath, AppIndicator3.IndicatorCategory.OTHER);
-        self.ind.set_label('NSB (Drammen)', self.app);
+        currpath = os.path.dirname(os.path.realpath(__file__));
+        self.icon_small = currpath + '/img/nsb_small.png';
+        self.icon_large = currpath + '/img/nsb_large.png';
+
+        self.ind = AppIndicator3.Indicator.new(self.app, self.icon_small, AppIndicator3.IndicatorCategory.OTHER);
+        self.ind.set_label(('NSB (%s)' % self.getStation()), self.app);
         self.ind.set_status(AppIndicator3.IndicatorStatus.ACTIVE);
         self.ind.set_menu(self.create_menu());
 
         self.update = Thread(target = self.refreshPrice);
         self.update.setDaemon(True);
         self.update.start();
+
+        self.getStation();
 
     def create_menu(self):
         menu = Gtk.Menu();
@@ -33,29 +42,32 @@ class Indicator():
         menu.show_all();
         return menu;
 
-    def price(self):
-        api = 'https://blockchain.info/ticker';
+    def getStation(self):
+        api = ('http://reisapi.ruter.no/Place/GetStop/%i?callback=?' % self.station);
+
         with urllib.request.urlopen(api) as url:
-            data = json.loads(url.read().decode());
-            btc_usd = data['USD']['last'];
-            self.priceAlert(btc_usd);
-            label = 'BTC: $' + str(round(btc_usd));
-            return label;
+            api = url.read().decode();
+            api = api.replace('(', '');
+            api = api.replace(')', '');
+            api = api.replace('?', '');
+            api = api.replace(';', '');
+            data = json.loads(api);
+            district = data['District'];
+            return district;
 
     def refreshPrice(self):
         while True:
-            time.sleep(5);
-            GObject.idle_add(self.ind.set_label, self.price(), self.app, priority = GObject.PRIORITY_DEFAULT);
+            time.sleep(self.refresh_rate);
+            GObject.idle_add(self.ind.set_label, self.getStation(), self.app, priority = GObject.PRIORITY_DEFAULT);
 
     def priceAlert(self, price):
-        iconpath = self.currpath + '/img/nsb_large.png';
         minAlert = 9000;
         maxAlert = 11000;
 
         if(maxAlert <= price):
-            s.call(['notify-send', '-i', iconpath, 'Bitcoin Price Indicator', ('Hurray! Bitcoin price has reached $%s.' % round(price))]);
+            s.call(['notify-send', '-i', self.icon_large, 'Bitcoin Price Indicator', ('Hurray! Bitcoin price has reached $%s.' % round(price))]);
         elif(minAlert >= price):
-            s.call(['notify-send', '-i', iconpath, 'Bitcoin Price Indicator', ('Oh no! Bitcoin price has dropped to $%s.' % round(price))]);
+            s.call(['notify-send', '-i', self.icon_large, 'Bitcoin Price Indicator', ('Oh no! Bitcoin price has dropped to $%s.' % round(price))]);
 
     def github(self, source):
         webbrowser.open('https://github.com/sirajuddin97/NSBAlert');
