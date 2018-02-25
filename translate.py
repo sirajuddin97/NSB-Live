27ca20c37b2219049d37f793591c16717668ce82
Sirajuddin Asjad <sirasjad@gmail.com>
2018-02-25 02:05:17 +0100

    Update

---
+++ translate.py
@@ -0,0 +1,13 @@
+def translate(text, lang):
+    if lang == 'en':
+        if text == "departures": return "Departures";
+        if text == "settings": return "Settings";
+        if text == "open": return "GitHub";
+        if text == "quit": return "Quit";
+    elif lang == 'no':
+        if text == "departures": return "Avganger";
+        if text == "settings": return "Innstillinger";
+        if text == "open": return "GitHub";
+        if text == "quit": return "Avslutt";
+    else:
+        return "Invalid language!";
